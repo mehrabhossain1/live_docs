4cdb85c631b6c2d1791a126a4810c13c59a59cdd
mehrabhossain1 <mehrab.munna00@gmail.com>
2024-12-01 17:06:51 +0600

    Homepage

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,47 @@
-const Home = () => {
+import AddDocumentBtn from "@/components/AddDocumentBtn";
+import Header from "@/components/Header";
+import { SignedIn, UserButton } from "@clerk/nextjs";
+import { currentUser } from "@clerk/nextjs/server";
+import Image from "next/image";
+import { redirect } from "next/navigation";
+
+const Home = async () => {
+  // Current user using clerk
+  const clerkUser = await currentUser();
+  if (!clerkUser) redirect("/sign-in");
+
+  const documents = [];
+
   return (
-    <div>
-      <h1>This is page component</h1>
-    </div>
+    <main className="home-container">
+      <Header className="sticky top-0 left-0">
+        <div className="flex items-center gap-2 lg:gap-4">
+          Notification
+          <SignedIn>
+            <UserButton />
+          </SignedIn>
+        </div>
+      </Header>
+
+      {documents.length > 0 ? (
+        <div></div>
+      ) : (
+        <div className="document-list-empty">
+          <Image
+            src="/assets/icons/doc.svg"
+            alt="document"
+            width={40}
+            height={40}
+            className="mx-auto"
+          />
+
+          <AddDocumentBtn
+            userId={clerkUser.id}
+            email={clerkUser.emailAddresses[0].emailAddress}
+          />
+        </div>
+      )}
+    </main>
   );
 };
 
